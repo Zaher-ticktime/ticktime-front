1793ab86c7d71d0de7f48fcfeb8ffe2b1d22f5d8
Zaher Mostaghi <zahermostaghi@iMac-van-Zaher.home>
2025-08-04 19:23:47 +0200

    🟢 force deploy by minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Router, Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./contexts/AuthContext";
 import { AuthProvider } from "./contexts/AuthContext";
 import Layout from "./components/Layout";
